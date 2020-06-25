c9f573a23684c1e7b71d357173cfc0841bdf7209
ThanTrongLoi <thantrongloi648@gmail.com>
2020-06-25 20:58:00 +0700

    object and array

--- buoi1.js
+++ buoi1.js
@@ -15,6 +15,24 @@
 // var b
 // console.log('b = '+b) //b => undefined
 
+// 3: Object
+// object: đại diện cho 1 cá thể, class: đại diện cho 1 tập thể
+const teo = {
+//  key: value
+    name: 'Nguyen Van Teo',
+    age: 10,
+}
+console.log(teo.name, teo.age + " tuổi")
+console.log(teo['name'])
+// Trường hợp 2: Truy vấn tới key không tồn tại của object
+console.log(teo.address)
+
+// 4: Array
+const arrayNames = ["Teo", "Ti", "Tun"]
+arrayNames[0] = "Tuan"
+console.log(arrayNames[0])
+
+
 
 
 
