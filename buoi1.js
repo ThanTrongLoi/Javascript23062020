2b40581dc15f96616c5b9d6f52ce2c6ed4afa6f0
ThanTrongLoi <thantrongloi648@gmail.com>
2020-06-25 20:27:43 +0700

    khaibaobienvakieudulieu

---
+++ buoi1.js
@@ -0,0 +1,19 @@
+// 1: khai bao bien
+// var a = 5;
+// const b = 6;
+
+// a = 10;
+// b = 12;
+// console.log(a);
+// console.log(b);
+//const: mặc định, var: có thể thay đổi
+// 2: kiểu dữ liệu
+// Null, Undefined # null, undefined
+// null, undefined
+var a = null
+console.log('a = '+a)
+var b
+console.log('b = '+b) //b => undefined
+
+
+
