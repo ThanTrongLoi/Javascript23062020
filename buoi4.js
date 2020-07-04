16612e00df4608a74f7a713a3037221c66891731
ThanTrongLoi <thantrongloi648@gmail.com>
2020-07-04 20:19:08 +0700

    cách sử dụng từ khoá khai báo biến

---
+++ buoi4.js
@@ -0,0 +1,30 @@
+// 22: toán tử 3 ngôi: Ternary operator
+// const a = 5;
+// const b = 6;
+// if(a > b){
+//     console.log("A lon hon B")
+// }else{
+//     console.log("A be hon bang B")
+// }
+// ===============
+// console.log(a > b ? "A lon hon B" : "A be hon bang B") // toan tu 3 ngoi
+
+//23: Function Scope
+// var a = 5
+// function tinhtong(){
+//     var a = 7;
+//     var b = 6;
+//     return a + b
+// }
+// console.log(tinhtong())
+
+// var a = 1
+// function random(){
+//     var a = Math.random()
+//     console.log(a)
+// }
+// random()
+// console.log(a)
+
+//scope: phạm vi => khi biến trùng tên sẽ ưu tiên scope trong function trước
+// từ trong truy vấn từ ngoài đc còn ngoài không truy vấn từ trong đc 
